3dc7e7bdd1a71dd0bd49d5c088371504d3ec7478
yussan <main.yussan@gmail.com>
2022-05-19 19:32:03 +0700

    initial repo

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { queryToObj } from "string-manager"
-
-// redux
-import reducers from "./redux/reducers/index";
-import sagas from "./redux/sagas/index";
-import createSagaMiddleware from "redux-saga";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import ReduxLogger from "redux-logger";
-
-// pages
-import HomePage from "./pages/index";
-import NewsDetail from "./pages/NewsDetail";
-
-// redux configuration
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  reducers,
-  applyMiddleware(sagaMiddleware, ReduxLogger)
-);
-
-sagaMiddleware.run(sagas);
 
 const App = () => {
-  const query = queryToObj(window.location.search.replace("?", ""));
-  // const query = {};
   return (
-    <Provider store={store}>
-      {/* simple conditional to get post list or post detail */}
-      {query.id ? <NewsDetail /> : <HomePage />}
-    </Provider>
+    <>
+      <h1>Mastering React Hooks</h1>
+      <small>youtube.com/maucoding</small>
+
+      <div class="mb-5 mt-5">Margin bottom</div>
+    </>
   );
 };
 
